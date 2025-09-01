a8d9d93596c5c434e299f3d0150b3f8920c3ba98
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-09-01 18:54:27 +0200

    Update data_load.sql

--- scripts/bronze/data_load.sql
+++ scripts/bronze/data_load.sql
@@ -3,7 +3,7 @@
             WARNING !!!
 =====================================
 Error 'Permission denied' while uploading data. Paste the code below in pgAdmin4, PSQL Tool to load data manually:
-  \copy bronze.ess_mnp_info FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\mnp.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
+  \copy bronze.ess_mnp FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\mnp.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
   \copy bronze.ess_inm FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\inm.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
   \copy bronze.wfm_employees FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\wfm.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
 */
