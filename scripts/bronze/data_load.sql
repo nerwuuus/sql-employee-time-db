066754cd1bcee8db64a5ea5c99493c021ba13eca
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-09-01 18:54:07 +0200

    Update data_load.sql

--- scripts/bronze/data_load.sql
+++ scripts/bronze/data_load.sql
@@ -3,9 +3,9 @@
             WARNING !!!
 =====================================
 Error 'Permission denied' while uploading data. Paste the code below in pgAdmin4, PSQL Tool to load data manually:
-  \copy bronze.ess_mnp_info FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\mnp.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-  \copy bronze.ess_inm FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\inm.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-  \copy bronze.wfm_employees FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\wfm.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+  \copy bronze.ess_mnp_info FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\mnp.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
+  \copy bronze.ess_inm FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\inm.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
+  \copy bronze.wfm_employees FROM 'C:\Users\a817628\OneDrive - ATOS\Desktop\wfm.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8');
 */
 
 -- Perform the full load on bronze ess and wfm tables
