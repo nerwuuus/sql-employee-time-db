fc58044826a0be290cc8c8eb0e583d0c0c53551b
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-09-01 23:08:55 +0200

    Update procedure_truncate_and_load_silver.sql

--- scripts/silver/procedure_truncate_and_load_silver.sql
+++ scripts/silver/procedure_truncate_and_load_silver.sql
@@ -1,62 +1,98 @@
-/*
-============================================================================
-Stored Procedure: Load Silver Layer (Bronze -> Silver)
-============================================================================
-Script Purpose:
-  This stored procedure performs the ETL (Extract, Transform, Load) process to
-  populate the 'silver' schema tables from the 'bronze' schema.
-Actions Performed:
-  - Truncates Silver tables.
-  - Inserts transformed and cleansed data from Bronze into Silver tables.
-============================================================================
-*/
-
--- Invoke a procedure
-CALL silver.load_silver();
-
 CREATE OR REPLACE PROCEDURE silver.load_silver()
-LANGUAGE pgplsql
+LANGUAGE plpgsql
 AS $$
 BEGIN
-    TRUNCATE TABLE silver.ess_mnp;
-    INSERT INTO silver.ess_mnp (
-      name VARCHAR(255),
-      nessie INT,
-      date DATE,
-      status VARCHAR(255),
-      wbs VARCHAR(255),
-      wbs_description VARCHAR(255),
-      hours NUMERIC(4,2)  
-    )
-    SELECT 
-        -- Here will be added columns after data cleaning
-    FROM bronze.ess_mnp;
+TRUNCATE TABLE silver.ess_inm;
+INSERT INTO silver.ess_inm (
+    name,
+    nessie,
+    date,
+    status,
+    wbs,
+    wbs_description,
+    hours
+)
+SELECT
+    TRIM(name) AS name, -- Remove whitespaces
+    COALESCE(nessie, 0) AS nessie, -- Replace NULLs with 0
+    date,
+    CASE
+        WHEN status = 'Approval rejected' THEN 'Rejected'
+        WHEN status = 'In process' THEN 'Not released'
+        WHEN status = 'Released for approval' THEN 'Released for approval'
+        ELSE 'Approved'
+    END AS status, -- Normalize status field with clearer, user-friendly labels
+    COALESCE(wbs, 'N/A') AS wbs,
+    COALESCE(wbs_description, 'N/A') AS wbs_description,
+    CASE
+        WHEN COALESCE(hours, 0) <= 0 THEN 0
+        ELSE hours
+    END AS hours
+FROM bronze.ess_inm
+WHERE COALESCE(hours, 0) != 0;
 
-    TRUNCATE TABLE silver.ess_inm;
-    INSERT INTO silver.ess_inm (
-      name VARCHAR(255),
-      nessie INT,
-      date DATE,
-      status VARCHAR(255),
-      wbs VARCHAR(255),
-      wbs_description VARCHAR(255),
-      hours NUMERIC(4,2)
-    )
-    SELECT
-        -- Here will be added columns after data cleaning
-    FROM bronze.ess_inm;
 
-    TRUNCATE TABLE silver.wfm_employees;
-    INSERT INTO silver.wfm_employees (
-      name VARCHAR(50),
-      nessie INT,
-      hourly_rate NUMERIC(4,2),
-      GCM_level INT,
-      position VARCHAR(50)     
-    )
-    SELECT
-        -- Here will be added columns after data cleaning
-    FROM silver.wfm_employees;
+TRUNCATE TABLE silver.ess_mnp;
+INSERT INTO silver.ess_mnp (
+    name,
+    nessie,
+    date,
+    status,
+    wbs,
+    wbs_description,
+    hours
+)
+SELECT
+    TRIM(name) AS name, -- Remove whitespaces
+    COALESCE(nessie, 0) AS nessie, -- Replace NULLs with 0
+    date,
+    CASE  
+        WHEN status = 'Approval rejected' THEN 'Rejected'
+        WHEN status = 'In process' THEN 'Not released'
+        WHEN status = 'Released for approval' THEN status
+        ELSE 'Approved'
+    END AS status, -- Normalize status field with clearer, user-friendly labels
+    COALESCE(wbs, 'N/A') AS wbs,
+    COALESCE(wbs_description, 'N/A') AS wbs_description,
+    CASE
+        WHEN COALESCE(hours, 0) <= 0 THEN 0
+        ELSE hours
+    END AS hours
+FROM bronze.ess_mnp
+WHERE COALESCE(hours, 0) != 0;
+
+
+TRUNCATE TABLE silver.wfm_employees;
+INSERT INTO silver.wfm_employees (
+    nessie,
+    internal_or_external,
+    name,
+    gcm_level,
+    hourly_rate,
+    competence 
+)
+SELECT
+    nessie,
+    CASE
+        WHEN internal_or_external NOT IN ('External', 'Internal') THEN 'Unknown'
+        ELSE internal_or_external
+    END AS internal_or_external,
+    name,
+    gcm_level,
+    hourly_rate,
+    TRIM(SPLIT_PART( -- Extract the part before the first comma from a string
+        CASE
+            WHEN competence IS NULL THEN 'Unknown'
+            ELSE competence
+        END,
+        ',',
+        1
+    )) AS competence
+FROM bronze.wfm_employees
+WHERE
+    nessie IS NOT NULL 
+    AND gcm_level IS NOT NULL 
+    AND hourly_rate IS NOT NULL;
 
     -- Custom message
     RAISE NOTICE 'Silver tables have been successfully updated.';
