a6de25fdace9b4a219542bc1ab2e7cc613263179
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-09-01 18:36:28 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,43 @@
+/*
 
+============================================================================
+DDL Script: Create Silver Tables
+============================================================================
+Script Purpose:
+  This script creates tables in the 'silver' schema, dropping existing tables
+  if they already exist.
+Run this script to redefine the DDL structure of 'silver' Tables.
+============================================================================
+
+*/
+
+DROP TABLE IF EXISTS silver.ess_mnp;
+CREATE TABLE silver.ess_mnp (
+    name VARCHAR(255),
+    nessie INT,
+    date DATE,
+    status VARCHAR(255),
+    wbs VARCHAR(255),
+    wbs_description VARCHAR(255),
+    hours NUMERIC(4,2)
+); 
+
+DROP TABLE IF EXISTS silver.ess_inm;
+CREATE TABLE silver.ess_inm(
+    name VARCHAR(255),
+    nessie INT,
+    date DATE,
+    status VARCHAR(255),
+    wbs VARCHAR(255),
+    wbs_description VARCHAR(255),
+    hours NUMERIC(4,2)
+);
+
+DROP TABLE IF EXISTS silver.wfm_employees;
+CREATE TABLE silver.wfm_employees (
+    name VARCHAR(50),
+    nessie INT,
+    hourly_rate NUMERIC(4,2),
+    GCM_level INT,
+    position VARCHAR(50)
+);
