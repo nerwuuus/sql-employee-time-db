9bc6ab572390899b7b1797fac81ddfe47bd83ba9
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-09-02 19:41:25 +0200

    updated table names

--- tests/silver_quality_checks.sql
+++ tests/silver_quality_checks.sql
@@ -20,54 +20,54 @@ Usage Notes:
 -- Check for leading or trailing whitespaces in the 'name' column
 -- Expected result: no result
 SELECT name
-FROM bronze.ess_inm
+FROM bronze.sap_inm
 WHERE name != TRIM(name);
 
 SELECT name
-FROM bronze.ess_mnp
+FROM bronze.sap_mnp
 WHERE name != TRIM(name);
 
 
 /* Data standardisation & consistency in the low cardinality columns */
 -- Expectation: no results
 SELECT DISTINCT status
-FROM bronze.ess_mnp; 
+FROM bronze.sap_mnp; 
 
 SELECT DISTINCT status
-FROM bronze.ess_inm;
+FROM bronze.sap_inm;
 
 
 -- Check high cardinality columns for NULLs
 -- Expectation: no results
 SELECT DISTINCT wbs
-FROM bronze.ess_mnp
+FROM bronze.sap_mnp
 WHERE wbs IS NULL;
 
 SELECT DISTINCT wbs
-FROM bronze.ess_inm
+FROM bronze.sap_inm
 WHERE wbs IS NULL;
 
 SELECT DISTINCT wbs_description
-FROM bronze.ess_inm
+FROM bronze.sap_inm
 WHERE wbs_description IS NULL;
 
 SELECT DISTINCT wbs_description
-FROM bronze.ess_mnp
+FROM bronze.sap_mnp
 WHERE wbs_description IS NULL; 
 
 SELECT DISTINCT nessie
-FROM bronze.ess_inm
+FROM bronze.sap_inm
 WHERE nessie IS NULL;
 
 SELECT DISTINCT nessie
-FROM bronze.ess_mnp
+FROM bronze.sap_mnp
 WHERE nessie IS NULL;
 
 
 -- Check for NULLs or negative numbers
 -- Expectation: no result
 SELECT hours
-FROM bronze.ess_inm
+FROM bronze.sap_inm
 WHERE 
     hours IS NULL 
     OR hours > 24 
@@ -75,7 +75,7 @@ WHERE
     OR hours = 0; 
 
 SELECT hours
-FROM bronze.ess_mnp
+FROM bronze.sap_mnp
 WHERE 
     hours IS NULL 
     OR hours > 24 
@@ -85,7 +85,7 @@ WHERE
 -- Check for invalid dates
 -- Expectation: no result
 SELECT date
-FROM bronze.ess_inm
+FROM bronze.sap_inm
 WHERE
     date <= DATE '1900-01-01' OR
     LENGTH(TO_CHAR(date, 'YYYYMMDD')) != 8 OR -- Converts the date to a string to check its length
@@ -93,7 +93,7 @@ WHERE
     date < TO_DATE('20220101', 'YYYYMMDD');
 
 SELECT date
-FROM bronze.ess_mnp
+FROM bronze.sap_mnp
 WHERE
     date <= DATE '1900-01-01' OR
     LENGTH(TO_CHAR(date, 'YYYYMMDD')) != 8 OR -- Converts the date to a string to check its length
